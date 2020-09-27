7e0baeedf428a0c63c133250d793b1c45071b438
Tomoya KASHIMADA <tomoya@sforzando.co.jp>
2020-09-27 20:59:04 +0900

    :bug: Fix type

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,5 +1,5 @@
 declare module "*.vue" {
   import { defineComponent } from "vue";
-  const component: ReturnType<typeof defineComponent>;
+  const component: defineComponent;
   export default component;
 }
