729e0b5a61f327d8945c34065bdf23af8263eb39
Tomoya KASHIMADA <tomoya@sforzando.co.jp>
2021-04-17 15:47:19 +0900

    wip: :construction:  check auto deploy

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <p>Deploy test with Cloudflare Pages on push</p>
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
